b687b684426997214e3c3a2c14cc391f1f836978
Kala Foods <guhan.delta@gmail.com>
2021-01-31 00:34:48 -0500

    Initial Commit - Boilerplate Code with Initial Setup and dependencies installed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import { ChatEngine } from 'react-chat-engine';
-import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <ChatEngine
-          height="100vh"
-          projectID=""
-          userName=""
-          userSecret=""
-      />
+     <h1>App Works!!..........</h1>
     </div>
   );
 }
