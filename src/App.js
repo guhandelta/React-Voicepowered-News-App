ad8e7784631e206e76be2b5dda02e74c2f8b4534
Kala Foods <guhan.delta@gmail.com>
2021-01-31 03:33:38 -0500

    Connected teh React App with the Alan account and enabled Alan AI on the React App using the AlanBtn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { useEffect } from 'react'
+import alanBtn  from '@alan-ai/alan-sdk-web'
+
+const alanKey = 'a59849212bfc741cb6fe82405a3b4d642e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 const App = () => {
+
+  useEffect(() => {
+    // This will display the Alan button on the page
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) =>{
+        // switch or if can be used here
+          if(command === 'testCommand'){
+            alert('App Works');
+          }
+      }
+    })
+  }, []);
+
   return (
     <div className="App">
      <h1>App Works!!..........</h1>
