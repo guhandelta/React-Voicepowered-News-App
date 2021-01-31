d9da85646add95299b67b32a1b252cbf0088eed1
Kala Foods <guhan.delta@gmail.com>
2021-01-31 11:18:48 -0500

    Used Material-UI features to wrap teh NewsCard with some interactive animation components - Facing issues with Grow/@material-ui

--- src/components/NewsCards/index.js
+++ src/components/NewsCards/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { Grid, Grow } from '@material-ui/core'
+
 import  NewsCard  from '../NewsCard'
 
 const NewsCards = ({ articles }) => {
     return (
-        <div>
-            {articles.map((article, i) => (
-                <NewsCard article={article} key={i} />
-            ))}
-        </div>
-    )
+        <Grow in> {/* If true, show the components, triggers the enter or exit animation */}
+            <Grid container alignItems="stretch" spacing={3}>
+                {articles.map((article, i) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
+                        <NewsCard article={article} key={i} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Grow>
+)
 }
 
 export default NewsCards
